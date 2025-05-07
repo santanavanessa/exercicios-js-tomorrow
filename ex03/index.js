b88a1d757a8dc82636bd4b566f3e0fcfa5df517d
Vanessa Santana <santanavanessa91@gmail.com>
2025-05-06 22:01:42 -0300

    initial commit

--- ex03/index.js
+++ ex03/index.js
@@ -1,12 +1,3 @@
-/*
-
-    Listando os seis primeiros números primos: 2, 3, 5, 7, 11 e 13, podemos ver 
-    que o sexto primo é 13. Qual é o 1001º número primo? Faça um programa 
-    JavaScript que apresente esse número para o usuário.
-
-*/
-
-
 let primos = 0;
 for (n = 2; primos < 1001; n++) {
   let numeroPrimo = true;
